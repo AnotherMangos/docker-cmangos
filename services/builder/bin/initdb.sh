5350dd1f1a79b6fb3f540b068a1feeb6239d055f
cedric <cedric.thomas@epitech.eu>
2019-10-28 21:57:34 -0400

    auto sql

--- services/builder/bin/initdb.sh
+++ services/builder/bin/initdb.sh
@@ -5,7 +5,10 @@ cd / && \
 wait-for-mysql.sh && \
 git clone $GITHUB_DATABASE classicdb && \
 git clone $GITHUB_CMANGOS mangos && \
+git clone $GITHUB_CMANGOS api && \
 cp /runtime/etc/InstallFullDB.config /classicdb/ && \
+mysql -uroot -h $MYSQL_HOST -P $MYSQL_PORT -p$MYSQL_ROOT_PASSWORD < /api/sql/create_database.sql && \
+mysql -uroot -h $MYSQL_HOST -P $MYSQL_PORT -p$MYSQL_ROOT_PASSWORD cmangosapi < /api/sql/create_tables.sql && \
 mysql -uroot -h $MYSQL_HOST -P $MYSQL_PORT -p$MYSQL_ROOT_PASSWORD < /mangos/sql/create/db_create_mysql.sql && \
 mysql -uroot -h $MYSQL_HOST -P $MYSQL_PORT -p$MYSQL_ROOT_PASSWORD classicmangos < /mangos/sql/base/mangos.sql && \
 mysql -uroot -h $MYSQL_HOST -P $MYSQL_PORT -p$MYSQL_ROOT_PASSWORD classiccharacters < /mangos/sql/base/characters.sql && \
